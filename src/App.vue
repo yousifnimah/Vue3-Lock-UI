1940b5c13e8eb6301c2010694f041fcd60655437
Yousif N.Abbass <yousifnimah5@gmail.com>
2023-08-01 17:13:44 +0300

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
-<template>
-  <div>
-    <VLockUI Password="1234">
-      <h1>Hello</h1>
-    </VLockUI>
-  </div>
-</template>
-
 <script setup lang="ts">
-import VLockUI from "@/components/VLockUI/VLockUI.vue";
-import {defineComponent} from "vue";
 
-defineComponent({
-  name: "App",
-  components: {VLockUI}
-})
 </script>
 
-<style lang="scss"></style>
+<template>
+<div>
+  <h2>hello</h2>
+</div>
+</template>
+
+<style scoped>
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import {App, Plugin} from 'vue'
 import VLockUI from './components/VLockUI/VLockUI.vue'
-
-
-export default {
-    install(Vue: App) {
-        Vue.component(VLockUI.name, VLockUI)
-    }
-} as Plugin
 
 export {VLockUI}
